3b802f29bec081397b9d6faa3e7bf13d49858811
Aruzo <dawid12391111@gmail.com>
2022-04-14 12:36:39 +0200

    ui changes

--- frontend/components/Categories.tsx
+++ frontend/components/Categories.tsx
@@ -11,21 +11,26 @@ const Categories = ({ categories }: { categories: ICategory[] }) => {
         >
           <h2 className="font-semibold text-3xl">{category.name}</h2>
           <p className="text-neutral-200">{category.description}</p>
-          {category.subCategories?.map((subCategory) => (
-            <Link key={subCategory.id} href={`/sub-category/${subCategory.id}`}>
-              <a className="flex justify-between items-center rounded-lg outline outline-8 outline-neutral-800 hover:outline-neutral-700 hover:bg-neutral-700 transition-all">
-                <h3 className="font-medium text-lg text-brand-400">
-                  {subCategory.name}
-                </h3>
-                <div className="flex flex-col items-center">
-                  <h4 className="font-medium text-lg text-brand-400">
-                    {subCategory.threadsCount}
-                  </h4>
-                  <p className="text-sm">Threads</p>
-                </div>
-              </a>
-            </Link>
-          ))}
+          <div>
+            {category.subCategories?.map((subCategory) => (
+              <Link
+                key={subCategory.id}
+                href={`/sub-category/${subCategory.id}`}
+              >
+                <a className="flex justify-between items-center p-2 rounded-lg hover:bg-neutral-700 transition-colors">
+                  <h3 className="font-medium text-lg text-brand-400">
+                    {subCategory.name}
+                  </h3>
+                  <div className="flex flex-col items-center">
+                    <h4 className="font-medium text-lg text-brand-400">
+                      {subCategory.threadsCount}
+                    </h4>
+                    <p className="text-sm">Threads</p>
+                  </div>
+                </a>
+              </Link>
+            ))}
+          </div>
         </div>
       ))}
     </>
