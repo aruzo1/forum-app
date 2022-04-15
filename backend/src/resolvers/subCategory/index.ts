67d808dab23f1d2dcf056821725720cc7f989d40
Aruzo <dawid12391111@gmail.com>
2022-04-16 00:18:46 +0200

    added sub category queries

--- backend/src/resolvers/subCategory/index.ts
+++ backend/src/resolvers/subCategory/index.ts
@@ -1,8 +1,18 @@
-import { FieldResolver, Int, Resolver, Root } from "type-graphql";
+import { FieldResolver, Int, Resolver, Root, Query, Arg } from "type-graphql";
 import { SubCategory, Thread } from "../../entities";
 
 @Resolver(() => SubCategory)
 export default class SubCategoryResolver {
+  @Query(() => [SubCategory])
+  subCategories() {
+    return SubCategory.find();
+  }
+
+  @Query(() => SubCategory)
+  subCategory(@Arg("id") id: string) {
+    return SubCategory.findOneBy({ id });
+  }
+
   @FieldResolver(() => [Thread])
   threads(@Root() { id }: SubCategory) {
     return Thread.findBy({ subCategoryId: id });
