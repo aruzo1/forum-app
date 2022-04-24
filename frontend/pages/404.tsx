cc8f332357b846c6c4cf3c412e380e17e396da57
Aruzo <dawid12391111@gmail.com>
2022-04-24 21:37:20 +0200

    updated 404 page title

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,8 +1,12 @@
 import { NextPage } from "next";
+import Head from "next/head";
 import Error from "components/Error";
 
 const Page404: NextPage = () => (
   <>
+    <Head>
+      <title>Forum - Not Found</title>
+    </Head>
     <Error title="404" msg="Page not found." />
   </>
 );
