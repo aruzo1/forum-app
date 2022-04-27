b3a2611832b8a473765c6c0c9e27da2b1fdf814e
Aruzo <dawid12391111@gmail.com>
2022-04-27 15:46:38 +0200

    removed dummy icons

--- frontend/components/Thread.tsx
+++ frontend/components/Thread.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import { IThread } from "lib/types";
 import Avatar from "components/ui/Avatar";
-import Comment from "public/icons/comment.svg";
-import Heart from "public/icons/heart.svg";
 
 const Thread = ({ thread }: { thread: IThread }) => (
   <div className="card col-span-full flex flex-col gap-y-4">
@@ -19,20 +17,6 @@ const Thread = ({ thread }: { thread: IThread }) => (
       {thread.title}
     </h1>
     <p className="break-words">{thread.body}</p>
-    <div className="flex gap-x-4">
-      <button className="flex items-center gap-x-2 group">
-        <Heart className="fill-brand-400 group-hover:fill-brand-300 transition-colors" />
-        <span className="font-medium text-brand-400 group-hover:text-brand-300 transition-colors">
-          1
-        </span>
-      </button>
-      <button className="flex items-center gap-x-2 group">
-        <Comment className="fill-brand-400 group-hover:fill-brand-300 transition-colors" />
-        <span className="font-medium text-brand-400 group-hover:text-brand-300 transition-colors">
-          3
-        </span>
-      </button>
-    </div>
   </div>
 );
 
